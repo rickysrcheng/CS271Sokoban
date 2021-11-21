da611d96bc7c24a9f7077717d7f7fd6adbeab6a6
Emily <gallage1@uci.edu>
2021-11-20 21:31:37 -0800

    Added Sokoban.py, preprocess, all relevant txt files

---
+++ Sokoban.py
@@ -0,0 +1,65 @@
+import argparse
+from preprocess import *
+
+class Sokoban_Board:
+	def __init__(self):
+		self.board = None
+		self.sizeH = 0
+		self.sizeV = 0
+		self.walls = []
+		self.boxes = []
+		self.storage = []
+		self.playerX = 0
+		self.playerY = 0
+# Reads command line and expects the input file as type .txt
+def parse_args():
+	parser = argparse.ArgumentParser(description='Solves a Sokoban game using an RL algorithm.')
+	parser.add_argument('input_file', metavar='FILE', nargs=1,
+	                    help='input file of type txt')
+	args = parser.parse_args()
+	if args.input_file[0][-4:] != '.txt':
+		print("Input file must be a txt file.")
+		args = None
+	return args
+
+# Reads the input file, line by line, and stores the formatted results in the board object
+def read_input(args):
+	with open(args.input_file[0], 'r') as file:
+		s_board = Sokoban_Board()
+		size_line = file.readline().split()
+		s_board.sizeH, s_board.sizeV = int(size_line[0]), int(size_line[1])
+		s_board.board = [[' ' for _ in range(s_board.sizeV)] for _ in range(s_board.sizeH)]
+		s_board.walls = parse_input_line(file.readline().split(), s_board.board, '#')
+		s_board.boxes = parse_input_line(file.readline().split(), s_board.board, '$')
+		s_board.storage = parse_input_line(file.readline().split(), s_board.board, '.')
+		player_line = file.readline().split()
+		s_board.playerX, s_board.playerY = int(player_line[0]), int(player_line[1])
+		s_board.board[s_board.playerX - 1][s_board.playerY - 1] = '@'
+		return s_board
+# For each line in file, parses the list of values and returns the coordinate pairs as a list
+# Also "draws" sokoban board
+def parse_input_line(line, board, symbol):
+	line_len = int(line[0]) * 2
+	coordinates = []
+	for i in range(0, line_len, 2):
+		row = int(line[i + 1])
+		col = int(line[i + 2])
+		board[row - 1][col - 1] = symbol
+		coordinate = [row, col]
+		coordinates.append(coordinate)
+	return coordinates
+# Executes input reading
+args = parse_args()
+if args:
+	s_board = read_input(args)
+	print("board_size: ", s_board.sizeH, " ", s_board.sizeV)
+	print("walls: ", s_board.walls)
+	print("boxes: ", s_board.boxes)
+	print("storages: ", s_board.storage)
+	print("player: ", s_board.playerX, " ", s_board.playerY)
+	print()
+	# Prints out sokoban board
+	for i in range(s_board.sizeH):
+		print(''.join(s_board.board[i]))
+
+	preprocess(s_board.sizeH, s_board.sizeV, s_board.walls, s_board.boxes, s_board.storage)
---
+++ preprocess.py
@@ -0,0 +1,26 @@
+import numpy as np
+WALL = -1000
+FLOOR = -1
+CORNER = -1100
+GOAL = 1000
+
+def preprocess(rows, columns, walls, boxes, goal):
+    board = np.full((rows, columns), FLOOR)
+    for r in range(1, rows + 1):
+        for c in range(1, columns + 1):
+            if([r,c] in walls):
+                board[r - 1, c - 1] = WALL
+            elif([r,c] in goal):
+                board[r - 1, c - 1] = GOAL
+            #check if corners
+            elif [r + 1, c] in walls and [r, c + 1] in walls:
+                board[r-1, c-1] = CORNER
+            elif [r - 1, c] in walls and [r, c + 1] in walls:
+                board[r-1, c-1] = CORNER
+            elif [r - 1, c] in walls and [r, c - 1] in walls:
+                board[r-1, c-1] = CORNER
+            elif [r, c - 1] in walls and [r + 1, c] in walls:
+                board[r-1, c-1] = CORNER
+    print(board)
+    return board
+
